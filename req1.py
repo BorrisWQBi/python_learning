b7f1a06171564cf37385846b284b98d4b26a9c72
Borris.Bi <biweiquan@lakala.com>
2019-06-26 18:13:23 +0800

    no message

--- req1.py
+++ req1.py
@@ -3,11 +3,10 @@ from bs4 import BeautifulSoup
 import requests
 
 if __name__=="__main__":
-    #target = "http://gitbook.cn/"
     target = "https://www.biqukan.com/1_1094/5403177.html"
     req = requests.get(target)
     html = req.text
     bf = BeautifulSoup(html)
     texts = bf.find_all("div",id="content")
-    v1 = texts[0].text.encode("iso-8859-1")
-    print(v1.decode("gbk"))
+    v1 = texts[0].text.replace('         ', '\n')#.encode("iso-8859-1")
+    print(v1)
